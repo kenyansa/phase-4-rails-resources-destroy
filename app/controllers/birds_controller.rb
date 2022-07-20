5d314c1bdbafd04b03d8781391ece0a194857a22
Kenyansa <kenyansajustine@gmail.com>
2022-07-20 21:50:37 +0300

    completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -43,6 +43,18 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+  
+  #DELETE /birds/:id/
+  def destroy
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy
+      head :no_content
+      # render json: true
+    else
+      render json: {error: "Bird not found"}, status: :not_found
+    end
+  end
 
   private
 
